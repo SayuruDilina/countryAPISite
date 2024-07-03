6a809c3e3207590475f3af70b864255678add58e
Sayuru Dilina <tumiraduli@gmail.com>
2024-07-03 14:11:52 +0530

    second commit

--- app.js
+++ app.js
@@ -19,3 +19,29 @@ fetch("https://restcountries.com/v3.1/all")
 
     tblCountries.innerHTML=tblBody;
 })
+function serchCuntrie(){
+    let searchValue=document.getElementById("txtSearchValue").value;
+
+    let offitalName = document.getElementById("offitalName");
+   let name = document.getElementById("name")
+   let img =  document.getElementById("img")
+    let capital=document.getElementById("capital")   
+
+    console.log(searchValue);
+    fetch(`https://restcountries.com/v3.1/name/${searchValue}`)
+    .then(res=>res.json())
+    .then(data =>{
+
+        data.forEach(obj=>{
+            offitalName.innerText = obj.name.official;
+            name.innerText = obj.name.common;
+             capital.innerText=  obj.capital; 
+            img.innerHTML=`<img src="${obj.flags.png}" alt="">`
+        })
+        console.log(data);
+
+
+
+    
+    })
+}
